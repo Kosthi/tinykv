e19fb9f96e3da32e6ef3225878bb4a37ad62026c
Koschei <nitianzero@gmail.com>
2024-10-25 19:44:50 +0800

    feat: 实现 Raft 领导人选举

--- raft/raft.go
+++ raft/raft.go
@@ -16,9 +16,13 @@ package raft
 
 import (
 	"errors"
+	"fmt"
+	"math/rand"
 
 	pb "github.com/pingcap-incubator/tinykv/proto/pkg/eraftpb"
 )
+
+const Debug = false
 
 // None is a placeholder node ID used when there is no leader.
 // None 是当没有领导者时使用的占位符节点 ID。
@@ -139,7 +143,7 @@ type Raft struct {
 	// 当前任期
 	Term uint64
 
-	// 得到的选票
+	// 选票投给谁
 	Vote uint64
 
 	// the log
@@ -163,7 +167,7 @@ type Raft struct {
 	msgs []pb.Message
 
 	// the leader id
-	// 领导者 ID
+	// 领导者 ID，没有为 None
 	Lead uint64
 
 	// heartbeat interval, should send
@@ -209,6 +213,9 @@ type Raft struct {
 	// 仅在领导者的已应用索引大于该值时才允许提议配置更改。
 	// (用于 3A 配置更改)
 	PendingConfIndex uint64
+
+	// 每个跟随者是否对心跳消息作出回复 2A
+	heartbeatResp map[uint64]bool
 }
 
 // newRaft return a raft peer with the given config
@@ -218,7 +225,42 @@ func newRaft(c *Config) *Raft {
 		panic(err.Error())
 	}
 	// Your Code Here (2A).
-	return nil
+	raftLog := newLog(c.Storage)
+	hardState, confState, err := c.Storage.InitialState()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if c.peers == nil {
+		c.peers = confState.Nodes
+	}
+
+	prs := make(map[uint64]*Progress, len(c.peers))
+	for _, p := range c.peers {
+		prs[p] = &Progress{
+			Match: 0,
+			Next:  0,
+		}
+	}
+
+	return &Raft{
+		id:               c.ID,
+		Term:             hardState.Term,
+		Vote:             hardState.Vote,
+		RaftLog:          raftLog,
+		Prs:              prs,
+		State:            StateFollower,
+		votes:            make(map[uint64]bool),
+		msgs:             nil,
+		Lead:             0,
+		heartbeatTimeout: c.HeartbeatTick,
+		electionTimeout:  c.ElectionTick,
+		heartbeatElapsed: 0,
+		electionElapsed:  0,
+		leadTransferee:   0,
+		PendingConfIndex: 0,
+		heartbeatResp:    make(map[uint64]bool),
+	}
 }
 
 // sendAppend sends an append RPC with new entries (if any) and the
@@ -234,24 +276,174 @@ func (r *Raft) sendAppend(to uint64) bool {
 // sendHeartbeat 向指定节点发送心跳 RPC。
 func (r *Raft) sendHeartbeat(to uint64) {
 	// Your Code Here (2A).
+	msg := pb.Message{
+		MsgType: pb.MessageType_MsgHeartbeat, // 注意 MsgHeartbeat 才是领导者发给跟随者的心跳消息类型
+		To:      to,
+		From:    r.id,
+		Term:    r.Term,
+	}
+
+	// 添加心跳消息到待发送消息队列
+	r.msgs = append(r.msgs, msg)
+}
+
+// sendAllHeartbeat 给所有其他节点发送心跳 RPC，仅 leader 可用。
+func (r *Raft) sendAllHeartbeat() {
+	for peer := range r.Prs {
+		if peer != r.id {
+			r.sendHeartbeat(peer)
+		}
+	}
+}
+
+// HeartbeatResponse 发送心跳回复 RPC
+func (r *Raft) sendHeartbeatResponse(to uint64) {
+	r.msgs = append(r.msgs, pb.Message{
+		MsgType: pb.MessageType_MsgHeartbeatResponse,
+		To:      to,
+		Term:    r.Term,
+	})
+}
+
+func (r *Raft) startElection() {
+	// 当集群只有一个节点时，不需要选举，直接成为领导者
+	if len(r.Prs) == 1 {
+		r.Term++
+		r.becomeLeader()
+	} else {
+		// 否则成为候选人，发起选举
+		r.becomeCandidate()
+		if Debug {
+			fmt.Printf("%x start election at term %d\n", r.id, r.Term)
+		}
+		r.sendAllRequestVote()
+	}
+}
+
+// sendRequestVote 向某节点发起投票请求
+func (r *Raft) sendRequestVote(to uint64) {
+	msg := pb.Message{
+		MsgType: pb.MessageType_MsgRequestVote,
+		To:      to,
+		From:    r.id,
+		Term:    r.Term,
+	}
+
+	r.msgs = append(r.msgs, msg)
+}
+
+func (r *Raft) sendAllRequestVote() {
+	// 向其他所有节点发送投票请求
+	for peer := range r.Prs {
+		if peer != r.id {
+			r.sendRequestVote(peer)
+			if Debug {
+				fmt.Printf("%x send requestVote to %x at term %d\n", r.id, peer, r.Term)
+			}
+		}
+	}
+}
+
+func (r *Raft) sendRequestVoteResponse(to uint64, reject bool) {
+	msg := pb.Message{
+		MsgType: pb.MessageType_MsgRequestVoteResponse,
+		To:      to,
+		From:    r.id,
+		Term:    r.Term,
+		Reject:  reject,
+	}
+	r.msgs = append(r.msgs, msg)
 }
 
 // tick advances the internal logical clock by a single tick.
 // tick 在内部逻辑时钟上推进一个 tick。
 func (r *Raft) tick() {
-	// Your Code Here (2A).
+	// 增加选举计时，对于领导人需要增加心跳计时
+	r.electionElapsed++
+
+	switch r.State {
+	case StateFollower:
+		if r.electionElapsed >= r.electionTimeout {
+			r.electionElapsed = 0
+			err := r.Step(pb.Message{MsgType: pb.MessageType_MsgHup})
+			if err != nil {
+				return
+			}
+		}
+	case StateCandidate:
+		if r.electionElapsed >= r.electionTimeout {
+			r.electionElapsed = 0
+			err := r.Step(pb.Message{MsgType: pb.MessageType_MsgHup})
+			if err != nil {
+				return
+			}
+		}
+	case StateLeader:
+		// 如果是领导者还要增加心跳计时
+		r.heartbeatElapsed++
+		// 如果心跳超时，发送心跳
+		if r.heartbeatElapsed >= r.heartbeatTimeout {
+			r.heartbeatElapsed = 0
+			err := r.Step(pb.Message{MsgType: pb.MessageType_MsgBeat})
+			if err != nil {
+				return
+			}
+		}
+		// 如果选举超时，判断是否发起新一轮选举
+		if r.electionElapsed >= r.electionTimeout {
+			r.electionElapsed = 0
+
+			// 计算心跳回应数
+			heartbeatRespNum := len(r.heartbeatResp)
+			r.heartbeatResp = make(map[uint64]bool)
+			r.heartbeatResp[r.id] = true
+
+			// 心跳回应数不超过一半，成为孤岛，应该重新发起选举
+			if heartbeatRespNum < len(r.Prs)/2 {
+				r.startElection()
+			}
+		}
+	}
+}
+
+// randomElectionTimeout 重置选举超时，根据测试，结果应该在 [10, 20) 之间
+func (r *Raft) randomElectionTimeout() int {
+	return 10 + rand.Intn(10)
+}
+
+// reset by new term
+func (r *Raft) reset(term uint64, vote uint64, state StateType, lead uint64) {
+	r.Term = term
+	r.Vote = vote
+	r.State = state
+	r.votes = make(map[uint64]bool)
+	r.Lead = lead
+	r.electionTimeout = r.randomElectionTimeout() // 随机选举超时时间
+	r.heartbeatElapsed = 0
+	r.electionElapsed = 0
+	r.leadTransferee = None
+	r.heartbeatResp = make(map[uint64]bool)
+	r.heartbeatResp[r.id] = true
 }
 
 // becomeFollower transform this peer's state to Follower
 // becomeFollower 将该节点的状态转换为跟随者。
 func (r *Raft) becomeFollower(term uint64, lead uint64) {
-	// Your Code Here (2A).
+	r.reset(term, None, StateFollower, lead)
+	if Debug {
+		fmt.Printf("%x became follower at term %d\n", r.id, r.Term)
+	}
 }
 
 // becomeCandidate transform this peer's state to candidate
 // becomeCandidate 将该节点的状态转换为候选者。
 func (r *Raft) becomeCandidate() {
-	// Your Code Here (2A).
+	// 当前节点任期自增，成为候选人，并给自己投票
+	r.reset(r.Term+1, r.id, StateCandidate, None)
+	r.votes[r.id] = true
+	if Debug {
+		fmt.Printf("%x became candidate at term %d\n", r.id, r.Term)
+	}
 }
 
 // becomeLeader transform this peer's state to leader
@@ -260,31 +452,297 @@ func (r *Raft) becomeLeader() {
 	// Your Code Here (2A).
 	// NOTE: Leader should propose a noop entry on its term
 	// 注意：领导者应在其任期内提议一个无操作条目（noop entry）。
+	// 自己成为领导者
+	r.reset(r.Term, None, StateLeader, r.id)
+	if Debug {
+		fmt.Printf("%x became leader at term %d\n", r.id, r.Term)
+	}
+	// 发送心跳给所有的其他节点
+	r.sendAllHeartbeat()
 }
 
 // Step the entrance of handle message, see `MessageType`
 // on `eraftpb.proto` for what msgs should be handled
 // Step 是处理消息的入口，考虑 `eraftpb` 中的 `MessageType` 了解应处理哪些消息。
 func (r *Raft) Step(m pb.Message) error {
-	// Your Code Here (2A).
+	var err error = nil
 	switch r.State {
 	case StateFollower:
+		err = r.StepFollower(m)
 	case StateCandidate:
+		err = r.StepCandidate(m)
 	case StateLeader:
+		err = r.StepLeader(m)
 	}
-	return nil
+	return err
+}
+
+// Step the entrance of handle message, see `MessageType`
+// on `eraftpb.proto` for what msgs should be handled
+// Step 是处理消息的入口，考虑 `eraftpb` 中的 `MessageType` 了解应处理哪些消息。
+func (r *Raft) StepFollower(m pb.Message) error {
+	var err error = nil
+	switch m.MsgType {
+	case pb.MessageType_MsgHup: // 跟随者发现选举超时，任期自增，重置状态信息，成为候选者，然后发起选举
+		r.startElection()
+	case pb.MessageType_MsgBeat: // 仅领导者处理
+	case pb.MessageType_MsgPropose: // 暂不处理
+	case pb.MessageType_MsgAppend: // 跟随者处理来自领导者的追加条目消息
+		r.handleAppendEntries(m)
+	case pb.MessageType_MsgAppendResponse: // 仅领导者处理
+	case pb.MessageType_MsgRequestVote: // 跟随者处理来自其他候选者节点的选举投票请求
+		r.handleRequestVote(m)
+	case pb.MessageType_MsgRequestVoteResponse: // 仅候选者处理
+	case pb.MessageType_MsgSnapshot: // 暂不处理
+	case pb.MessageType_MsgHeartbeat: // 跟随者处理来自领导者的心跳消息
+		r.handleHeartbeat(m)
+	case pb.MessageType_MsgHeartbeatResponse: // 仅领导者处理
+	case pb.MessageType_MsgTransferLeader: // 暂不处理
+	case pb.MessageType_MsgTimeoutNow: // 暂不处理
+	}
+	return err
+}
+
+// Step the entrance of handle message, see `MessageType`
+// on `eraftpb.proto` for what msgs should be handled
+// Step 是处理消息的入口，考虑 `eraftpb` 中的 `MessageType` 了解应处理哪些消息。
+func (r *Raft) StepCandidate(m pb.Message) error {
+	var err error = nil
+	switch m.MsgType {
+	case pb.MessageType_MsgHup: // 候选者发现选举超时，任期自增，重置状态信息，然后发起选举
+		r.startElection()
+	case pb.MessageType_MsgBeat: // 仅领导者处理
+	case pb.MessageType_MsgPropose: // 暂不处理
+	case pb.MessageType_MsgAppend: // 候选者处理来自其他节点的追加条目请求
+		r.handleAppendEntries(m)
+	case pb.MessageType_MsgAppendResponse: // 仅领导者处理
+	case pb.MessageType_MsgRequestVote: // 候选者处理来自其他候选者节点的选举投票请求
+		r.handleRequestVote(m)
+	case pb.MessageType_MsgRequestVoteResponse: // 候选者处理其他节点的选票请求回复
+		r.handleRequestVoteResponse(m)
+	case pb.MessageType_MsgSnapshot: // 暂不处理
+	case pb.MessageType_MsgHeartbeat: // 候选者处理来自某任期领导者的心跳消息
+		r.handleHeartbeat(m)
+	case pb.MessageType_MsgHeartbeatResponse: // 仅领导者处理
+	case pb.MessageType_MsgTransferLeader: // 暂不处理
+	case pb.MessageType_MsgTimeoutNow: // 暂不处理
+	}
+	return err
+}
+
+// Step the entrance of handle message, see `MessageType`
+// on `eraftpb.proto` for what msgs should be handled
+// Step 是处理消息的入口，考虑 `eraftpb` 中的 `MessageType` 了解应处理哪些消息。
+func (r *Raft) StepLeader(m pb.Message) error {
+	var err error = nil
+	switch m.MsgType {
+	case pb.MessageType_MsgHup: // 领导者一般不会发起选举
+	case pb.MessageType_MsgBeat: // 领导者接收 Beat 消息向其他节点发生心跳
+		r.sendAllHeartbeat()
+	case pb.MessageType_MsgPropose: // 暂不处理
+	case pb.MessageType_MsgAppend: // 领导者处理追加条目请求，可能是其他任期的领导者发过来的
+		r.handleAppendEntries(m)
+	case pb.MessageType_MsgAppendResponse: // 领导者处理追加条目消息回复
+		r.handleAppendResponse(m)
+	case pb.MessageType_MsgRequestVote: // 领导者处理选举投票请求，其他候选者节点发过来的
+		r.handleRequestVote(m)
+	case pb.MessageType_MsgRequestVoteResponse: // 只有候选者处理选举投票回复
+	case pb.MessageType_MsgSnapshot: // 暂不处理
+	case pb.MessageType_MsgHeartbeat: // 领导者处理心跳消息，可能是其他任期的领导者发过来的
+		r.handleHeartbeat(m)
+	case pb.MessageType_MsgHeartbeatResponse: // 领导者处理心跳消息回复，记录哪些节点还存活
+		r.handleHeartbeatResponse(m)
+	case pb.MessageType_MsgTransferLeader: // 暂不处理
+	case pb.MessageType_MsgTimeoutNow: // 暂不处理
+	}
+	return err
 }
 
 // handleAppendEntries handle AppendEntries RPC request
 // handleAppendEntries 处理追加条目 RPC 请求。
 func (r *Raft) handleAppendEntries(m pb.Message) {
 	// Your Code Here (2A).
+	if Debug {
+		fmt.Printf("%x receive append from %x\n", r.id, m.From)
+	}
+
+	// 检查接收到的领导者任期是否小于当前任期
+	if m.Term < r.Term {
+		// 如果领导者任期较小，返回一个失败的响应
+		r.msgs = append(r.msgs, pb.Message{
+			MsgType: pb.MessageType_MsgAppendResponse,
+			To:      m.From,
+			From:    r.id,
+			Term:    r.Term,
+			Reject:  true,
+		})
+		return
+	}
+
+	// 当前节点是跟随者，更新当前任期和领导者
+	// 当前节点非跟随者，重置状态，成为跟随者
+	if r.State == StateFollower {
+		r.Term = m.Term
+		r.Lead = m.From
+	} else {
+		r.becomeFollower(m.Term, m.From)
+	}
+
+	// 发送追加条目成功响应
+	r.msgs = append(r.msgs, pb.Message{
+		MsgType: pb.MessageType_MsgAppendResponse,
+		To:      m.From,
+		From:    r.id,
+		Term:    r.Term,
+		Reject:  false,
+	})
+}
+
+// handleAppendEntries handle AppendEntries RPC request
+// handleAppendEntries 处理追加条目 RPC 请求回复。
+func (r *Raft) handleAppendResponse(m pb.Message) {
+	// Your Code Here (2A).
+	if Debug {
+		fmt.Printf("%x receive append from %x\n", r.id, m.From)
+	}
+
+	// 检查接收到的领导者任期是否小于当前任期
+	if m.Term < r.Term {
+		// 如果领导者任期较小，返回一个失败的响应
+		r.msgs = append(r.msgs, pb.Message{
+			MsgType: pb.MessageType_MsgAppendResponse,
+			To:      m.From,
+			From:    r.id,
+			Term:    r.Term,
+			Reject:  true,
+		})
+		return
+	}
+
+	// 当前节点是跟随者，更新当前任期和领导者
+	// 当前节点非跟随者，重置状态，成为跟随者
+	if r.State == StateFollower {
+		r.Term = m.Term
+		r.Lead = m.From
+	} else {
+		r.becomeFollower(m.Term, m.From)
+	}
+
+	// 发送追加条目成功响应
+	r.msgs = append(r.msgs, pb.Message{
+		MsgType: pb.MessageType_MsgAppendResponse,
+		To:      m.From,
+		From:    r.id,
+		Term:    r.Term,
+		Reject:  false,
+	})
 }
 
 // handleHeartbeat handle Heartbeat RPC request
 // handleHeartbeat 处理心跳 RPC 请求。
 func (r *Raft) handleHeartbeat(m pb.Message) {
 	// Your Code Here (2A).
+	if Debug {
+		fmt.Printf("%x receive hearbeat from %x\n", r.id, m.From)
+	}
+
+	// 检查接收到的任期
+	if m.Term < r.Term {
+		// 返回一个过期的心跳响应
+		r.msgs = append(r.msgs, pb.Message{
+			MsgType: pb.MessageType_MsgHeartbeatResponse,
+			To:      m.From,
+			Term:    r.Term,
+		})
+		return
+	}
+
+	// 当前节点是跟随者，更新当前任期和领导者，重置选举时间
+	// 当前节点非跟随者，重置状态，成为跟随者
+	if r.State == StateFollower {
+		r.Term = m.Term
+		r.Lead = m.From
+		r.electionElapsed = 0
+	} else {
+		r.becomeFollower(m.Term, m.From)
+	}
+
+	// 发送心跳响应
+	r.msgs = append(r.msgs, pb.Message{
+		MsgType: pb.MessageType_MsgHeartbeatResponse,
+		To:      m.From,
+		Term:    r.Term,
+	})
+}
+
+// handleHeartbeatResponse handle Heartbeat RPC request
+// handleHeartbeatResponse 处理心跳 RPC 请求回复。
+func (r *Raft) handleHeartbeatResponse(m pb.Message) {
+	// Your Code Here (2A).
+	if Debug {
+		fmt.Printf("%x receive heartbeatResponse from %x\n", r.id, m.From)
+	}
+
+	// 检查接收到的任期
+	if m.Term > r.Term {
+		// 接收到的任期比当前节点任期大
+		r.Term = m.Term
+		if r.State != StateFollower {
+			r.becomeFollower(m.Term, m.From)
+		}
+	} else {
+		// 记录该节点对心跳响应作出了回复
+		r.heartbeatResp[m.From] = true
+	}
+}
+
+// handleRequestVote handle RequestVote RPC request
+// handleRequestVote 处理请求投票 RPC 请求。
+func (r *Raft) handleRequestVote(m pb.Message) {
+	// 检查接收到的任期
+	if m.Term < r.Term {
+		// 该选举投票请求已经过期，拒绝投票
+		r.sendRequestVoteResponse(m.From, true)
+		return
+	}
+
+	// 请求选票节点的任期比当前节点大
+	if m.Term > r.Term {
+		// 更新任期，重置选票
+		// 当前任期可能还没有选出领导者，设置为 None
+		r.becomeFollower(m.Term, None)
+	}
+
+	// 在相同的任期内，已经投给了其他节点，拒绝投票
+	if r.Vote != None && r.Vote != m.From {
+		r.sendRequestVoteResponse(m.From, true)
+		return
+	}
+
+	// 投票给这个节点
+	r.Vote = m.From
+	r.sendRequestVoteResponse(m.From, false)
+}
+
+// handleRequestVoteResponse handle RequestVote RPC response
+// handleRequestVoteResponse 处理请求投票 RPC 回复。
+func (r *Raft) handleRequestVoteResponse(m pb.Message) {
+	// 更新当前节点的投票状态
+	r.votes[m.From] = !m.Reject
+
+	// 统计赞成票和反对票
+	approvedVotes := 0
+	for _, hasApproved := range r.votes {
+		if hasApproved {
+			approvedVotes++
+		}
+	}
+
+	// 判断是否可以成为领导者
+	totalNodes := len(r.Prs)
+	if approvedVotes > totalNodes/2 {
+		r.becomeLeader()
+	}
 }
 
 // handleSnapshot handle Snapshot RPC request
