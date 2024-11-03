1076195e1f26e9391750cbecdc48cd4b9d3e947a
Koschei <nitianzero@gmail.com>
2024-11-03 17:21:40 +0800

    docs: engines 新增中文注释

--- kv/util/engine_util/doc.go
+++ kv/util/engine_util/doc.go
@@ -15,3 +15,17 @@ engine_util includes the following packages:
 * write_batch: code to batch writes into a single, atomic 'transaction'.
 * cf_iterator: code to iterate over a whole column family in badger.
 */
+
+/*
+Engine 是一个低级系统，用于本地存储键/值对（不支持分布或任何事务等）。该包包含与此类引擎交互的代码。
+
+CF 代表“列族”。对列族的良好描述可以在 https://github.com/facebook/rocksdb/wiki/Column-Families 找到
+（具体是针对 RocksDB 的，但一般概念是普遍适用的）。简而言之，列族是一个键命名空间。
+多个列族通常被实现为几乎独立的数据库。重要的是，每个列族可以单独配置。写入操作可以在列族之间原子执行，这是在独立数据库中无法做到的。
+
+engine_util 包含以下子包：
+
+* engines：一个数据结构，用于存储 unistore 所需的引擎。
+* write_batch：将多条写入操作批量处理为单个原子“事务”的代码。
+* cf_iterator：在 Badger 中迭代整个列族的代码。
+*/
--- kv/util/engine_util/engines.go
+++ kv/util/engine_util/engines.go
@@ -10,16 +10,25 @@ import (
 // Engines keeps references to and data for the engines used by unistore.
 // All engines are badger key/value databases.
 // the Path fields are the filesystem path to where the data is stored.
+// Engines 维护了用于 unistore 的引擎的引用和数据。
+// 所有引擎都是基于 Badger 的键值数据库。
+// Path 字段是数据存储在文件系统中的路径。
 type Engines struct {
 	// Data, including data which is committed (i.e., committed across other nodes) and un-committed (i.e., only present
 	// locally).
-	Kv     *badger.DB
+	// Kv 引擎，可以视为 Raft 论文中提到的状态机，用于存储数据，包括已提交（即跨其他节点已提交）和未提交（即仅存在于本地）数据
+	// 跨节点提交的数据：客户端的命令；本地数据：RaftApplyState 和 RegionLocalState
+	Kv *badger.DB
+	// Kv 引擎的数据存储路径
 	KvPath string
 	// Metadata used by Raft.
-	Raft     *badger.DB
+	// Raft 引擎，用于存储 Raft 元数据，包括日志和 RaftLocalState
+	Raft *badger.DB
+	// Raft 引擎的数据存储路径
 	RaftPath string
 }
 
+// NewEngines 创建并返回一个新的 Engines 实例。
 func NewEngines(kvEngine, raftEngine *badger.DB, kvPath, raftPath string) *Engines {
 	return &Engines{
 		Kv:       kvEngine,
@@ -29,14 +38,17 @@ func NewEngines(kvEngine, raftEngine *badger.DB, kvPath, raftPath string) *Engin
 	}
 }
 
+// WriteKV 将一个写入批处理操作写入 Kv 引擎。
 func (en *Engines) WriteKV(wb *WriteBatch) error {
 	return wb.WriteToDB(en.Kv)
 }
 
+// WriteRaft 将一个写入批处理操作写入 Raft 引擎。
 func (en *Engines) WriteRaft(wb *WriteBatch) error {
 	return wb.WriteToDB(en.Raft)
 }
 
+// Close 关闭 Kv 和 Raft 引擎的数据库连接。
 func (en *Engines) Close() error {
 	dbs := []*badger.DB{en.Kv, en.Raft}
 	for _, db := range dbs {
@@ -50,6 +62,7 @@ func (en *Engines) Close() error {
 	return nil
 }
 
+// Destroy 关闭数据库并删除它们在文件系统中的数据。
 func (en *Engines) Destroy() error {
 	if err := en.Close(); err != nil {
 		return err
@@ -64,17 +77,23 @@ func (en *Engines) Destroy() error {
 }
 
 // CreateDB creates a new Badger DB on disk at path.
+// CreateDB 在指定路径上创建一个新的 Badger 数据库。
 func CreateDB(path string, raft bool) *badger.DB {
+	// 获取 Badger 的默认配置
 	opts := badger.DefaultOptions
 	if raft {
 		// Do not need to write blob for raft engine because it will be deleted soon.
+		// 不需要为 raft 引擎写入 blob，因为它很快就会被删除。
 		opts.ValueThreshold = 0
 	}
+	// 设置数据库目录
 	opts.Dir = path
+	// 设置值存储目录
 	opts.ValueDir = opts.Dir
 	if err := os.MkdirAll(opts.Dir, os.ModePerm); err != nil {
 		log.Fatal(err)
 	}
+	// 用给定配置打开数据库
 	db, err := badger.Open(opts)
 	if err != nil {
 		log.Fatal(err)
